3c93e3a2bec9198289444981801bfb2451a09291
Nicu Tofan <nicu.tofan@gmail.com>
2013-03-23 15:53:38 +0200

    Added binary test for insert; the test fails

--- tests/libraries/libdxfrw/tests/DRW_Insert_test.cpp
+++ tests/libraries/libdxfrw/tests/DRW_Insert_test.cpp
@@ -12,14 +12,37 @@
 
 #include	<gtest/gtest.h>
 #include	<libdwgr.h>
+#include	<QString>
+#include	<QFile>
 
-static unsigned char insert_OD[] = {
-	0x0
-};
+#include	"../support/bitsbuild.h"
+#include	"../support/drw_interface_ghost.h"
+#include	"../support/odsample.h"
+
+#undef	EXPECT_DOUBLE_EQ
+#define EXPECT_DOUBLE_EQ(a,b)	\
+	EXPECT_TRUE( qFuzzyCompare(a,b) )
+
 
 TEST(DRW_Insert, parseDwg) {
+	int off;
 	DRW_Insert	tst;
-	dwgBuffer 	buf((char*)insert_OD, sizeof(insert_OD) );
-	//tst.parseDwg( DRW::AC1014, &buf );
 	
+		
+	char		tst_bf[250];
+	
+	Q_ASSERT( sizeof(od_insert) < 250 );
+	off = addBits(0, tst_bf, 
+				  BIT2(0,0), /* raw short - type of the entry */
+				  DSZ_SHORT, _ od_insert_id,
+				  //DSZ_LONG, _ od_insert_len, /* length of the object (if version > AC1014 */
+				  DSZ_BYTE, _ 0x11,
+				  DSZ_BYTE, _ od_insert_hdl,
+				  BITS_STOP_MARKER );
+	off = addRawBytes( off, tst_bf, (char*)od_insert, sizeof(od_insert) );
+	dwgBuffer 	buf((char*)tst_bf, off/8+1);
+	
+	/** @ todo the test fails; test data may be corrupted */
+	EXPECT_TRUE( tst.parseDwg(DRW::AC1014, &buf) );
+
 }
