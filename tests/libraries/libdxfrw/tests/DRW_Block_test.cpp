ceabac7d6631dd723a801980444f6e3c7842a4af
Nicu Tofan <nicu.tofan@gmail.com>
2013-03-23 15:51:30 +0200

    Added binary test for block; the test fails

--- tests/libraries/libdxfrw/tests/DRW_Block_test.cpp
+++ tests/libraries/libdxfrw/tests/DRW_Block_test.cpp
@@ -12,14 +12,36 @@
 
 #include	<gtest/gtest.h>
 #include	<libdwgr.h>
+#include	<QString>
+#include	<QFile>
 
-static unsigned char block_OD[] = {
-	0x0
-};
+#include	"../support/bitsbuild.h"
+#include	"../support/drw_interface_ghost.h"
+#include	"../support/odsample.h"
+
+#undef	EXPECT_DOUBLE_EQ
+#define EXPECT_DOUBLE_EQ(a,b)	\
+	EXPECT_TRUE( qFuzzyCompare(a,b) )
+
 
 TEST(DRW_Block, parseDwg) {
+	int off;
 	DRW_Block	tst;
-	dwgBuffer 	buf((char*)block_OD, sizeof(block_OD) );
-	//tst.parseDwg( DRW::AC1014, &buf );
+	
+	char		tst_bf[250];
+	
+	Q_ASSERT( sizeof(od_block) < 250 );
+	off = addBits(0, tst_bf, 
+				  BIT2(0,0), /* raw short - type of the entry */
+				  DSZ_SHORT, _ od_block_id,
+				  //DSZ_LONG, _ od_block_len, /* length of the object (if version > AC1014 */
+				  DSZ_BYTE, _ 0x11,
+				  DSZ_BYTE, _ od_block_hdl,
+				  BITS_STOP_MARKER );
+	off = addRawBytes( off, tst_bf, (char*)od_block, sizeof(od_block) );
+	dwgBuffer 	buf((char*)tst_bf, off/8+1);
+	
+	/** @ todo the test fails; test data may be corrupted */
+	EXPECT_TRUE( tst.parseDwg(DRW::AC1014, &buf) );
 	
 }
